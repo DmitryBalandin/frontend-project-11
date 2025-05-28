b3577ec6ffcf1c3382468b7400de2dc9e78e40c2
DmitryBalandin <balandinqwerty1983@gmail.com>
2025-05-28 22:47:58 +0500

    step 5 +

--- src/js/app.js
+++ src/js/app.js
@@ -19,25 +19,51 @@ export default (i18) => {
       .then((value) => {
         return parserRss(value)
       })
-      .then((data) =>{
+      .then((data) => {
         const message = i18.t('success')
         const id = uniqueId();
-        const posts = data.posts.map((post) => {return {...post, id}});
-        watchedObject.listAddRssNews = {  [id]:{...data.feed, linkFeed:inputValue, id,},...state.listAddRssNews, }
+        const posts = data.posts.map((post) => { return { ...post, id } });
+        watchedObject.listAddRssNews = { [id]: { ...data.feed, linkFeed: inputValue, id, }, ...state.listAddRssNews, }
         watchedObject.feedbackRss = message;
         watchedObject.rssIsValid = true
         watchedObject.inputValue = '';
-        watchedObject.posts = [...posts, ...state.posts]
+        watchedObject.posts = [...posts, ...state.posts];
+        const arrayRss = Object.values(watchedObject.listAddRssNews).map(value => value.linkFeed);
+        if (arrayRss.length === 1) {
+          update()
+        }
       })
       .catch((e) => {
         const message = i18.t(e.message.key);
-          watchedObject.feedbackRss = message;
-          watchedObject.rssIsValid = false;
-          watchedObject.inputValue = inputValue;
+        watchedObject.feedbackRss = message;
+        watchedObject.rssIsValid = false;
+        watchedObject.inputValue = inputValue;
+
       })
 
 
+
   })
+}
+
+function update() {
+  const arrayUrslRss = Object.values(watchedObject.listAddRssNews).map(value => value.linkFeed);
+  console.log(arrayUrslRss);
+  Promise.allSettled(arrayUrslRss.map(url => queryRss(url)))
+    .then(results => {
+      return results.map((result) => {
+        if (result.status == "fulfilled") {
+          return parserRss(result.value)
+          // new Promise(() => { return parserRss(result.value) })
+          //   .then((value) => console.log(value));
+        }
+      })
+    })
+    .then((result) => Promise.allSettled(result.map(({feed,posts}) =>{
+      console.log(feed,posts)
+    }))
+    );
+  setTimeout(() => update(), 2000)
 }
 // 'https://buzzfeed.com/world.xml'
 //https://thecipherbrief.com/feed

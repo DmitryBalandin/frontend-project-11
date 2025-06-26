e36b70b8e2f90f530974d1542d79a082656b8e9e
DmitryBalandin <balandinqwerty1983@gmail.com>
2025-06-26 21:51:29 +0500

    add gitignore folder dist delete comments

--- src/js/watcher.js
+++ src/js/watcher.js
@@ -3,8 +3,6 @@ import initialState from './initial-state'
 import { renderErrors, renderMain } from './view'
 
 export const watchedObject = onChange(initialState, function () {
-  // renderErrors(state)
-  // renderMain(Object.values(state.feeds), state.posts, state.ui.seenPosts)
   renderErrors(watchedObject)
   renderMain(Object.values(watchedObject.feeds), watchedObject.posts, watchedObject.ui.seenPosts)
 })
